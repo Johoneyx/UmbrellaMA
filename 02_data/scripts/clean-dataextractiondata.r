5ccffedecad5cceff0e3126b547329b6012530d4
Johoneyx <johanna.m.groening@gmail.com>
2024-05-22 10:35:25 +0100

    start cleaning merged dataextractiondata, create first autho psycont and pubyear variable

--- 02_data/scripts/clean-dataextractiondata.r
+++ 02_data/scripts/clean-dataextractiondata.r
@@ -1,71 +1,51 @@
+library(readxl)
+library(tidyverse)
+library(writexl)
+library(ggplot2)
+library(dplyr)
+library(purrr)
+library(data.table)
+library(readxl)
+library(stringr)
 
+merged_df <-as.data.table(read_xlsx("C:/Users/johan/Documents/PhD/UmbrellaMA/02_data/mergeddata/merged_df.xlsx"))
 
+#create firstauthor variable
 merged_df$firstauthor <- ifelse(is.na(str_extract(merged_df$author, ".*(?= \\()")), merged_df$authoronly, str_extract(merged_df$author, ".*(?= \\()"))
 
-
-# Replace "L.Clausen" with "Clausen"
-merged_df$firstauthor <- sub("L.Clausen", "Clausen", merged_df$firstauthor)
-
-# Replace "Jennifer L. Seddon" with "Seddon"
-merged_df$firstauthor <- sub("Jennifer L. Seddon", "Seddon", merged_df$firstauthor)
+#to lowercase
+merged_df$firstauthor <-tolower(merged_df$firstauthor)
 
 
-library(dplyr)
+# Definining new names
+new_names <- c("l.clausen"= "clausen","jennifer l. seddon"= "seddon", "ferdindand"="ferdinand", "bergé(2016)" = "berge", "shimmelmann" = "schimmelmann", "j.m. stone"   =" stone", "r. emsley"= "emsley",   "kristine rømer thomsen" =  "romer thomsen", "isaac et al" ="isaac", "vandijk" ="van djik", "gonzalez-into" ="gonzalez-pinto", "auther  et al." = "auther", "miller et al" = "miller", "rössler" = "roessler")
+
+# Rename authors
+merged_df$firstauthor <- recode(merged_df$firstauthor, !!!new_names)
 
 
-merged_df$firstauthor
-
-merged_df$publicationyear
-
-
-
-merged_df$publicationyear <- ifelse(is.na(merged_df$year), 
+#create publicationyear variable
+merged_df$pubyear <- ifelse(is.na(merged_df$year), 
                                     str_extract(merged_df$author, "\\((\\d+)\\)"), 
                                     merged_df$year)
 
 
 # Remove all brackets
-merged_df$publicationyear <- gsub("\\(|\\)", "", merged_df$publicationyear)
+merged_df$pubyear <- gsub("\\(|\\)", "", merged_df$pubyear)
 
 
-# Define the new names for the values you want to rename
-new_names <- c("ferdindand"="ferdinand", "bergé(2016)" = "bergé", "shimmelmann" = "schimmelmann", "j.m.stone"   =" stone", "r. emsley"= "emsley",   "kristinerømerthomsen"=  "romerthomsen", "isaacetal" ="isaac", "vandijk" ="vandjik", "gonzalez-into" ="gonzalez-pinto")
+#create psycont (psychosiscontinuumpopulation) variable
+merged_df$psycontpop<- str_extract(merged_df$dt_name, "^[^_]*")
 
-# Rename the values
-merged_df$firstauthor <- recode(merged_df$firstauthor, !!!new_names)
 
-merged_df$publicationyear 
-
+merged_df$studycode <- paste((gsub("\\s", "", merged_df$firstauthor)), merged_df$pubyear, merged_df$psycontpop, sep = "_")
 
 
 
-merged_df$pop <- str_extract(merged_df$dt_name, "^[^_]*")
-
-merged_df$pop
-
-#clean whitespaces
-merged_df$firstauthor <- gsub("\\s", "", merged_df$firstauthor)
-merged_df$publicationyear <- gsub("\\s", "", merged_df$publicationyear)
-
-merged_df$key <- paste(merged_df$firstauthor, merged_df$publicationyear, merged_df$pop, sep = "_")
-
-#clean whitespaces
-merged_df$firstauthor <- gsub("\\s", "", merged_df$firstauthor)
-merged_df$publicationyear <- gsub("\\s", "", merged_df$publicationyear)
+studycode_unique <- unique(merged_df$studycode)
+studycode_unique
 
 
-#rename 
-new_names <- c( "ferdindand"="ferdinand", "bergé(2016)" = "bergé", "shimmelmann" = "schimmelmann", "j.m.stone"   =" stone", "r. emsley"= "emsley",   "kristinerømerthomsen"=  "romerthomsen", "isaacetal" ="isaac", "vandijk" ="vandjik", "gonzalez-into" ="gonzalez-pinto")
-
-keys_unique <- unique(merged_df$key)
-
-
-
-
-# Write to an Excel file
-# Write the dataframe to an Excel file
-write.xlsx(merged_df, "merged_df.xlsx")
-write_xlsx(keys_unique_df, "studylist_extracteddata.xlsx")
 
 
 

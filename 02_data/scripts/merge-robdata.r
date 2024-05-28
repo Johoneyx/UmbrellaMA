263b4750c88562515ab29ffd3ee4b5ed4bc0a5dd
Johoneyx <johanna.m.groening@gmail.com>
2024-05-28 15:53:56 +0100

    read in all dataframes, as riccardo put the data I splitted the dataframes by reading in subdataframes only

--- 02_data/scripts/merge-robdata.r
+++ 02_data/scripts/merge-robdata.r
@@ -8,71 +8,103 @@ library(data.table)
 library(readxl)
 library(stringr)
 
-# Read the data using fread from data.table package and skip the first rows as the real headers are in the second row
-Rob_HP_S_R <- as.data.table(read_xlsx("C:/Users/johan/Documents/PhD/UmbrellaMA/02_data/rawdata/Rob_Assesment_Riccardo_Johanna21.09.2023.xlsx", sheet=1,skip=1))
+#Problem: Riccardo put the different studies next to each other instead of under each other thats why i only read in subframes so that i can merge them afterwards
+
+#riccardo rob cohort data healthy population symptoms
+Rob_HP_S_R_Cohort <- as.data.table(read_xlsx("C:/Users/johan/Documents/PhD/UmbrellaMA/02_data/rawdata/Rob_Assesment_Riccardo_Johanna21.09.2023.xlsx", sheet=1, range=cell_cols("A:L")))
 
 # Remove completely empty rows
-Rob_HP_S_R <- Rob_HP_S_R[!apply(is.na(Rob_HP_S_R), 1, all), ]
+Rob_HP_S_R_Cohort <- Rob_HP_S_R_Cohort [!apply(is.na(Rob_HP_S_R_Cohort), 1, all), ]
+
+View(Rob_HP_S_R_Cohort)
 
 
-View(Rob_HP_S_R)
-# Shorten the variable names
-names(Rob_HP_S_R) <- sub("\\..*$", "", names(Rob_HP_S_R))
-
-View(Rob_HP_S_R)
-
-
-#Problem: Riccardo put the different studies next to each other instead of under each other, grouped by studytype, I need to split the dataframes and merge them afterwards 
-
-# Get the column names
-col_names <- names(Rob_HP_S_R)
-
-# Find the indices of the columns that start with "Review"
-review_indices <- grep("^Review", col_names)
-
-# Get the index of the second "Review" column
-second_review_index <- review_indices[2]
-
-# Split the dataframe into two
-df1 <- Rob_HP_S_R[, 1:(second_review_index - 1)]
-df2 <- Rob_HP_S_R[, second_review_index:ncol(Rob_HP_S_R)]
-
-View(df1)
-View(df2)
-
-
-
-
-Rob_CHR_T_R <- as.data.table(read_xlsx("C:/Users/johan/Documents/PhD/UmbrellaMA/02_data/rawdata/Rob_Assesment_Riccardo_Johanna21.09.2023.xlsx", sheet=4,skip = 1))
+#riccardo rob cross-sectional data healthy population symptoms
+Rob_HP_S_R_CS<- as.data.table(read_xlsx("C:/Users/johan/Documents/PhD/UmbrellaMA/02_data/rawdata/Rob_Assesment_Riccardo_Johanna21.09.2023.xlsx", sheet=1, range=cell_cols("N:X")))
 
 # Remove completely empty rows
-Rob_CHR_T_R <- Rob_CHR_T_R [!apply(is.na(Rob_CHR_T_R ), 1, all), ]
+Rob_HP_S_R_CS <- Rob_HP_S_R_CS [!apply(is.na(Rob_HP_S_R_CS), 1, all), ]
 
-# Shorten the variable names
-names(Rob_CHR_T_R) <- sub("\\..*$", "", names(Rob_CHR_T_R))
+View(Rob_HP_S_R_CS)
 
-Rob_P_R <- as.data.table(read_xlsx("C:/Users/johan/Documents/PhD/UmbrellaMA/02_data/rawdata/Rob_Assesment_Riccardo_Johanna21.09.2023.xlsx", sheet=5,skip = 1))
-
-# Remove completely empty rows
-Rob_P_R <- Rob_P_R[!apply(is.na(Rob_P_R), 1, all), ]
-
-# Shorten the variable names
-names(Rob_P_R) <- sub("\\..*$", "", names(Rob_P_R))
-
-# Get the column names
-col_names <- names(Rob_P_R)
-
-# Find the indices of the columns that start with "Review"
-review_indices <- grep("^Review", col_names)
+#riccardo rob cohort data healthy population diagnosis
+Rob_HP_D_R_Cohort <- as.data.table(read_xlsx("C:/Users/johan/Documents/PhD/UmbrellaMA/02_data/rawdata/Rob_Assesment_Riccardo_Johanna21.09.2023.xlsx", sheet=2, range=cell_cols("A:L")))
 
 
-lapply(split_dfs, View)
+Rob_HP_D_R_Cohort <- Rob_HP_D_R_Cohort [!apply(is.na(Rob_HP_D_R_Cohort), 1, all), ]
 
-#
-Rob_P_C <- as.data.table(read_xlsx("C:/Users/johan/Documents/PhD/UmbrellaMA/02_data/rawdata/New_excel_Carolina_09.10_20.05.2024.xlsx", sheet=3,skip = 1))
+View(Rob_HP_D_R_Cohort)
+
+#riccardo rob cross-sectional data healthy population diagnosis
+Rob_HP_D_R_CS<- as.data.table(read_xlsx("C:/Users/johan/Documents/PhD/UmbrellaMA/02_data/rawdata/Rob_Assesment_Riccardo_Johanna21.09.2023.xlsx", sheet=2, range=cell_cols("N:X")))
 
 # Remove completely empty rows
-Rob_P_C <- Rob_P_C [!apply(is.na(Rob_P_C ), 1, all), ]
+Rob_HP_D_R_CS <- Rob_HP_D_R_CS [!apply(is.na(Rob_HP_D_R_CS), 1, all), ]
+
+View(Rob_HP_D_R_CS)
+
+
+#riccardo rob cohort data CHR population symptoms
+
+Rob_CHR_S_R_Cohort <- as.data.table(read_xlsx("C:/Users/johan/Documents/PhD/UmbrellaMA/02_data/rawdata/Rob_Assesment_Riccardo_Johanna21.09.2023.xlsx", sheet=3,range=cell_cols("A:L")))
+
+# Remove completely empty rows
+Rob_CHR_S_R_Cohort <- Rob_CHR_S_R_Cohort [!apply(is.na(Rob_CHR_S_R_Cohort ), 1, all), ]
+
+View(Rob_CHR_S_R_Cohort)
+
+#riccardo rob case-control data CHR population transition 
+
+Rob_CHR_S_R_CS <- as.data.table(read_xlsx("C:/Users/johan/Documents/PhD/UmbrellaMA/02_data/rawdata/Rob_Assesment_Riccardo_Johanna21.09.2023.xlsx", sheet=3, range=cell_cols("N:X")))
+
+# Remove completely empty rows
+Rob_CHR_S_R_CS  <- Rob_CHR_S_R_CS  [!apply(is.na(Rob_CHR_S_R_CS  ), 1, all), ]
+
+View(Rob_CHR_S_R_CS)
+#riccardo rob case-control data CHR population transition 
+
+Rob_CHR_S_R_CC <- as.data.table(read_xlsx("C:/Users/johan/Documents/PhD/UmbrellaMA/02_data/rawdata/Rob_Assesment_Riccardo_Johanna21.09.2023.xlsx", sheet=3, range=cell_cols("Z:AK")))
+
+# Remove completely empty rows
+Rob_CHR_S_R_CC  <- Rob_CHR_S_R_CC  [!apply(is.na(Rob_CHR_S_R_CC ), 1, all), ]
+
+View(Rob_CHR_S_R_CC)
+
+#riccardo rob cohort data CHR population transition 
+
+Rob_CHR_T_R_Cohort <- as.data.table(read_xlsx("C:/Users/johan/Documents/PhD/UmbrellaMA/02_data/rawdata/Rob_Assesment_Riccardo_Johanna21.09.2023.xlsx", sheet=4,range=cell_cols("A:L") ))
+
+# Remove completely empty rows
+Rob_CHR_T_R_Cohort <- Rob_CHR_T_R_Cohort [!apply(is.na(Rob_CHR_T_R_Cohort), 1, all), ]
+
+View(Rob_CHR_T_R_Cohort)
+
+#riccardo rob case-control data CHR population transition 
+
+Rob_CHR_T_R_CC <- as.data.table(read_xlsx("C:/Users/johan/Documents/PhD/UmbrellaMA/02_data/rawdata/Rob_Assesment_Riccardo_Johanna21.09.2023.xlsx", sheet=4,range=cell_cols("N:Y")))
+
+# Remove completely empty rows
+Rob_CHR_T_R_CC <- Rob_CHR_T_R_CC [!apply(is.na(Rob_CHR_T_R_CC), 1, all), ]
+
+View(Rob_CHR_T_R_CC)
+
+#rob riccardo psychosis population cross-sectional
+Rob_P_R_CS <- as.data.table(read_xlsx("C:/Users/johan/Documents/PhD/UmbrellaMA/02_data/rawdata/Rob_Assesment_Riccardo_Johanna21.09.2023.xlsx", sheet=5,range=cell_cols("A:K")))
+
+# Remove completely empty rows
+Rob_P_R_CS <- Rob_P_R_CS[!apply(is.na(Rob_P_R_CS), 1, all), ]
+
+View(Rob_P_R_CS)
+
+#rob riccardo psychosis population case-control
+Rob_P_R_CC <- as.data.table(read_xlsx("C:/Users/johan/Documents/PhD/UmbrellaMA/02_data/rawdata/Rob_Assesment_Riccardo_Johanna21.09.2023.xlsx", sheet=5,range=cell_cols("M:X")))
+
+# Remove completely empty rows
+Rob_P_R_CC <- Rob_P_R_CC[!apply(is.na(Rob_P_R_CC), 1, all), ]
+
+
+View(Rob_P_R_CC)
+
 
 
 

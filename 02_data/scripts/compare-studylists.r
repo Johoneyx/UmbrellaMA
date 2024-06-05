d1c0ddda48bfc51ea140861daae31814f3914147
Johoneyx <johanna.m.groening@gmail.com>
2024-06-05 15:42:50 +0100

    deleted murrie(2020) review from studylist and made an excel with those studies that are not extracted (studylist and extracted dataframe comparision) to doublecheck it manually

--- 02_data/scripts/compare-studylists.r
+++ 02_data/scripts/compare-studylists.r
@@ -7,7 +7,8 @@ library(writexl)
 
 # Read the Excel file
 studylist_evidencemap_unique <- read_excel("02_data/cleandata/df_studylist_cohort_unique.xlsx")
-merged_df_extracted<- read_excel("02_data/cleandata/merged_df_clean.xlsx")
+
+merged_df_clean<- read_excel("02_data/cleandata/merged_df_clean.xlsx")
 
 merged_df_code_unique <- unique(merged_df_clean$studycode)
 
@@ -18,5 +19,13 @@ studylist_evidencemap_comp <- studylist_evidencemap_unique %>%
     closest_match = merged_df_code_unique[stringdist::amatch(studycode, merged_df_code_unique, maxDist = Inf)]
   )
 
+#murrie 2020 sind studien die übergang von cannabis-induzierter psychose zu schiyophrenia prevalenzen berechnet
+  studylist_evidencemap_comp <- studylist_evidencemap_comp %>%
+  filter(reviews != "Murrie (2020)")
+
 write_xlsx(studylist_evidencemap_comp, "02_data/cleandata/studylist_evidencemap_comparision.xlsx")
 
+studies_to_check <- studylist_evidencemap_comp %>%
+filter(extracted == "no")
+
+write_xlsx(studies_to_check, "02_data/cleandata/studies_to_check.xlsx")

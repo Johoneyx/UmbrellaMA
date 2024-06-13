5463866359cbb74f23e91e423e016f3a0b7b0694
Johoneyx <johanna.m.groening@gmail.com>
2024-06-13 22:39:42 +0100

    clean columnvalues prospective cohort dataextractiondata

--- 02_data/scripts/clean-dataextractiondata_prospectivelongitudinal.r
+++ 02_data/scripts/clean-dataextractiondata_prospectivelongitudinal.r
@@ -82,7 +82,7 @@ df_plcohort <- df_plcohort %>%
 
 # Replace "frequent", "infrequent" with [word]
 df_plcohort <- df_plcohort %>%
-  mutate(level_coded = str_replace_all(level_coded, "(frequent|infrequent|occasional|abuse or dependence|abuse| abuse / dependence|dependence|misuse| mild or heavy|heavy|light|any|moderate|regular|substance use disorder|cud)", "[\\1]"))
+  mutate(level_coded = str_replace_all(level_coded, "(frequent|infrequent|occasional|abuse or dependence|abuse| abuse / dependence|dependence|misuse|mild or heavy|heavy|light|any|moderate|regular|substance use disorder|cud)", "[\\1]"))
 
 # Replace "times", "days" with (word)
 df_plcohort <- df_plcohort %>%
@@ -95,4 +95,183 @@ df_plcohort <- df_plcohort %>%
 #clean controlgroup variable 
 sort(levels(as.factor(df_plcohort$"level_coded")))
 
-View(df_plcohort)
+
+
+
+names(df_plcohort)
+
+#clean cannabis measure variable
+sort(levels(as.factor(df_plcohort$"cannabis measure")))
+
+df_plcohort$"cannabis measure"<-tolower(df_plcohort$"cannabis measure")
+
+#clean cannabis measure variable
+#sort(levels(as.factor(df_plcohort$"cannabis measure")))
+
+##df_plcohort <- df_plcohort %>%
+ #mutate(measure_coded ="cannabis measure") %>%
+  #mutate(measure_coded = str_replace_all#(measure_coded, "(|days|years)", "(\\1)"))
+
+
+  df_plcohort$"cannabis measure"<-tolower(df_plcohort$"cannabis measure")
+
+  df_plcohort <- df_plcohort %>%
+ mutate(measure_coded ="cannabis measure") %>%
+
+# Replace all spaces in column names with underscores
+# Replace all spaces in column names with underscores
+colnames(df_plcohort) <- gsub(" ", "_", colnames(df_plcohort))
+
+sort(levels(as.factor(df_plcohort$outcome)))
+
+  
+df_plcohort <- df_plcohort %>%
+ mutate(outcome_coded= outcome) 
+
+
+df_plcohort <- df_plcohort %>%
+  mutate(outcome_coded = str_replace_all(outcome coded, "(symtoms|symtomes|symptomes|symtpoms|symtomes|symptomes)", "symptoms"))
+
+
+# Print words starting with "sym" 
+df_plcohort %>%
+  filter(str_detect(outcome_coded, "\\bschiz\\w*")) %>%
+ pull(outcome_coded) %>%
+ unique() %>%
+ str_split(" ") %>%
+  unlist() %>%
+ .[str_detect(., "\\bschiz\\w*")] %>%
+ unique() %>%
+ print()
+
+df_plcohort <- df_plcohort %>%
+  mutate(outcome_coded = str_replace_all(`outcome_coded`, "(symptoms|symtpoms|symtoms|symtomes)", "symptoms"))
+
+
+  # Print words starting with "sym" 
+df_plcohort %>%
+  filter(str_detect(outcome_coded, "\\bgen\\w*")) %>%
+ pull(outcome_coded) %>%
+ unique() %>%
+ str_split(" ") %>%
+  unlist() %>%
+ .[str_detect(., "\\bgen\\w*")] %>%
+ unique() %>%
+ print()
+
+df_plcohort <- df_plcohort %>%
+  mutate(outcome_coded = str_replace_all(`outcome_coded`, "(dimensios|dimention)", "dimension"))%>%
+  mutate(outcome_coded = str_replace_all(`outcome_coded`, "(ngtaive|negtaive)", "negative"))
+
+
+table(as.factor(df_plcohort$outcome_coded))
+
+
+df_plcohort %>%
+  filter(str_detect(outcome_coded, "\\bpsych\\w*")) %>%
+  pull(outcome_coded) %>%
+  unique() %>%
+  print()
+
+#view levels as a table so that you can decide which expression to take and which values you have to rename
+View(as.data.frame(table(as.factor(df_plcohort$outcome_coded))))
+
+df_plcohort <- df_plcohort %>%
+  mutate(outcome_coded = str_replace_all(`outcome_coded`, "(negative psychosis|negative dimension|negative schizophrenia symptoms|negative symptoms)", "negative psychotic symptoms"))%>% 
+  mutate(outcome_coded = str_replace_all(`outcome_coded`, "negative psychotic symptoms of psychotic symptoms", "negative psychotic symptoms"))
+
+
+df_plcohort <- df_plcohort %>%
+  mutate(outcome_coded = str_replace_all(`outcome_coded`, "(negative psychosis|negative dimension|negative schizophrenia symptoms|negative symptoms)", "negative psychotic symptoms"))
+
+df_plcohort <- df_plcohort %>%
+  mutate(outcome_coded = str_replace_all(`outcome_coded`, "(positive dimension|positive psychosis|positive schziphrenia symptoms|positive symptoms)", "positive psychotic symptoms"))
+
+  #view levels as a table so that you can decide which expression to take and which values you have to rename
+View(as.data.frame(table(as.factor(df_plcohort$outcome_coded))))
+
+# Load the necessary package
+library(dplyr)
+
+# Create a table and filter it
+table_data <- as.data.frame(table(as.factor(df_plcohort$outcome_coded)))
+filtered_table_data <- filter(table_data, Freq > 5)
+
+# Display the filtered table
+View(filtered_table_data)
+
+df_plcohort <- df_plcohort %>%
+  mutate(outcome_coded = str_replace_all(`outcome_coded`, "(schizophrenia symptoms (0-58) |mean psychotic symptoms)", "psychotic symptoms"))
+
+
+df_plcohort <- df_plcohort %>%
+  mutate(outcome_coded = str_replace_all(`outcome_coded`, "(icd-8|psychosis onset|psychosis onset|non-affective psychosis)", "development of a psychotic disorder"))
+
+# Create a table from the outcome_coded column
+table_data <- as.data.frame(table(as.factor(df_plcohort$outcome_coded)))
+
+filtered_table_data <- filter(table_data, Freq < 5)
+# Display the filtered table
+View(filtered_table_data)
+
+# Create a table from the outcome_coded column
+table_data <- as.data.frame(table(as.factor(df_plcohort$country)))
+
+filtered_table_data <- filter(table_data, Freq > 3)
+# Display the filtered table
+View(filtered_table_data)
+
+names(df_plcohort)
+
+
+#variables who might have info psychosis type
+#fep_vs_chronic 
+#target_population
+#cohort
+#cohort_more_detail
+#diagnostic_tool
+#dt_name
+#kind_of_psychosis
+
+#cannabis
+#lifetime_cannabis_use
+#cannabis_level_of_use
+#comparision(control-group)
+#cannabis_measure
+#recall?
+
+
+#factors accounted for 
+#baseline_differences_between_group?
+#factors_accounted_for
+
+#age
+#age_sd
+#age_range
+#mean_age
+
+
+
+#time
+#follow-up_duration
+#cannabis__&outcome_analysis_timeframe
+#time_frame
+#recall?
+#followup_duration
+#study_type
+#survival_curve?
+
+#sample_size_(total_n)
+#total_n 
+
+
+#%_male_in_cannabis_group 
+#%_male_in_no_cannabis_group
+
+
+#reference
+#citation
+#title
+ 
+
+

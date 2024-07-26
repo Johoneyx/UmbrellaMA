dc94f462ff903227ade3a65e66c08583d086410b
Johoneyx <johanna.m.groening@gmail.com>
2024-07-26 12:40:22 +0100

    make a summary table to explore data

--- 02_data/scripts/clean-dataextractiondata_prospectivelongitudinal.r
+++ 02_data/scripts/clean-dataextractiondata_prospectivelongitudinal.r
@@ -174,7 +174,7 @@ filtered_table_comp <- filter(table_comp, Freq > 3)
 View(filtered_table_comp)
 
 
-
+#********************CANNABISCONTINUATION***********************
 
 
 
@@ -495,7 +495,7 @@ unique(df_plcohort$age_range)
 
 
 
-#********************POPULATION*****************
+#********************POPULATION*****************]
 
 df_plcohort <- df_plcohort %>%
     mutate(population = case_when(
@@ -571,4 +571,77 @@ dev.off()
 #*****************************************CLEAN STATISTICAL DATA******************************************
 
 #sample_size_(total_n)
-#total_n 
+#total_n
+#"n_outcome"
+#"n_no_outcome"
+#"n_cu"
+ #"n_ncu"
+#"cu_p"
+# "ncu_p"
+#"cu_np" "ncu_np" "or" "lci_or" "uci_or" "p_or" "rr" "lci_rr" "uci_rr" "p_rr" "hr" "lci_hr" "uci_hr" "p_hr" "timeframe_hr" "aor" "lci_aor" "uci_aor" "p_aor" "adjusted_factors_aor" "arr" "lci_arr" "uci_arr" "p_arr" "rr_direction" "adjusted_factors_arr" "ahr" "lci_ahr" "uci_ahr" "p_ahr""timeframe_ahr""covariates_ahr" "mean_in__outcome_group" "sd_in_outcome_group" "mean_in_no-outcome_group" "sd_in_no-outcome_group" "mean_c" "sd_c" "mean_nc" "sd_nc" "smd" "smd_measure" "lci_smd" "uci_smd"
+#"p(smd)"
+# "b"
+ #"se_b"
+ #"p_b"
+#"ab"
+# "se_ab"
+# "p_ab"
+# "covariates_ab"
+# "other_statistical_method"
+# "factor"
+# "statistical_parameter"
+# "p-value"
+# "factors_accounted_for"
+ #"time_frame"
+ #"%_outcome"
+#"or_direction"
+#"hr_direction"
+#"aor_direction"
+#"arr_direction"
+#"direction_ahr"
+#"statistical_method"
+#"extracted_from"
+#"corr"
+#"p_corr"
+#"acorr"
+#"p_acorr"
+#"covariates_acorr"
+#"%_of_chr_transition_to_fep_in_cannabis_grups"
+#"se_of_%_transition_cu"
+# "%_of_chr_transition_to_fep_in_no-cannabis_grups"
+#"se_of_%_transition_ncu"
+#"survival_curve?"
+#"icluded_in_meta-analysis"
+#"correlation_method"
+#"%_in_outcome_group"
+#"%_in_no-outcome_group"
+#"n_dcu"
+#"mean_dc"
+#"sd_dc"
+#"β_lci"
+#"β_uci"
+# "f-value"
+
+
+names(df_plcohort)
+
+#*******************CLEAN_ADJUSTED_FACTORS*********************
+
+
+
+#********************EXPLORE****************************************
+
+#very helpful table i could use this to decide whether study is relevant *e.g. plamondon incorporates metval metmet level does it make sense to combine those levels into one effectsize
+summary <- df_plcohort %>%
+group_by(studycode) %>%
+summarise(
+  outcomes =paste(unique(outcome_coded), collapse =" & "),
+  cannabis_levels = paste(unique(cannabis_level_of_use), collapse =" & "),
+  timepoints = paste(unique(followup), collapse =" & "),
+  populations = paste(unique(population), collapse =" & ")
+
+
+  
+)
+
+View(summary)

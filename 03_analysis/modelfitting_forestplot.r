52e128ded60d38282cb0eb4c05ebd9aa5b05c88d
Johoneyx <johanna.m.groening@gmail.com>
2024-10-30 06:19:35 +0000

    move files i dont need anymore to archive

--- 03_analysis/modelfitting_forestplot.r
+++ 03_analysis/modelfitting_forestplot.r
@@ -47,15 +47,11 @@ View(df_meta_analysis)
 #Forestplots
 ############################################
 
-
-                                      
-#*****************************Create_Forestplot_Continous_DATA****
-
+                               
 par(tck=-.01, mgp=c(1,0.01,0), mar=c(2,4,0,2))
 
 dd <- c(0,diff(df_meta_analysis$study))
 rows <- (1:rem_SMD_all$k) + cumsum(dd)
-
 
 
 forest(rem_SMD_all, rows=rows, ylim=c(2,max(rows)+3), xlim=c(-10,14), cex=0.8,
@@ -73,7 +69,6 @@ View(dat)
 agg <- aggregate(dat, cluster=study, V=vcov(rem_SMD_all, type="obs"), addk=TRUE)
 
 
-
 res_agg <- rma(yi, vi, method="EE", data=agg, digits=3)
 res_agg
 
@@ -81,7 +76,6 @@ res_agg
 forest(res_agg, xlim=c(-5,5), mlab="Pooled Estimate", header=TRUE, slab=studycode, ilab= ki, ilab.lab="Estimates",  ylim=c(-10,res_agg$k+20), order=population, rows=c(75:52,47:27,21:4))
 text(-5, c(76,48,22), c("CHR", "HP",
                         "P"), font=4, pos=4)
-
 
 
 
@@ -96,66 +90,12 @@ addpoly(res.chr, row=50.5)
 addpoly(res.hp, row= 24.5)
 addpoly(res.p, row= 1.5)
 
-### fit random-effects model in the three subgroups
-res.s <- rma(yi, vi, subset=(alloc=="systematic"), data=dat)
-res.r <- rma(yi, vi, subset=(alloc=="random"),     data=dat)
-res.a <- rma(yi, vi, subset=(alloc=="alternate"),  data=dat)
- 
-### add summary polygons for the three subgroups
-addpoly(res.s, row=18.5, mlab=mlabfun("RE Model for Subgroup", res.s))
-addpoly(res.r, row= 7.5, mlab=mlabfun("RE Model for Subgroup", res.r))
-addpoly(res.a, row= 1.5, mlab=mlabfun("RE Model for Subgroup", res.a))
+
 
 ############################################
 #   MODEL WITH HP, CHR, P Groups
 ############################################
 
-### a little helper function to add Q-test, I^2, and tau^2 estimate info
-mlabfun <- function(text, x) {
-   list(bquote(paste(.(text),
-      " (Q = ", .(fmtx(x$QE, digits=2)),
-      ", df = ", .(x$k - x$p), ", ",
-      .(fmtp2(x$QEp)), "; ",
-      I^2, " = ", .(fmtx(x$I2, digits=1)), "%, ",
-      tau^2, " = ", .(fmtx(x$tau2, digits=2)), ")")))}
- 
-### set up forest plot (with 2x2 table counts added; the 'rows' argument is
-### used to specify in which rows the outcomes will be plotted)
 
-forest(res_agg, xlim=c(-4,5), mlab="Pooled Estimate", header=TRUE, slab=studycode,ilab=ki, ilab.lab="Estimates", ilab.xpos=-2)
-
-### set font expansion factor (as in forest() above) and use a bold font
-op <- par(cex=0.40, font=2)
- 
-### add additional column headings to the plot
-#text(c(-9.5,-8,-6,-4.5), 54, c("CU_P", "CU_NP", "NCU_P", "NCU_NP"))
-#text(c(-8.75,-5.25),     55, c("Cannabis use", "Control"))
- 
-### switch to bold italic font
-par(font=4)
- 
-### add text for the subgroups
-text(-16, c(24,16,5), pos=4, c("HP",
-                               "CHR",
-                               "P"))
- 
-### set par back to the original settings
-par(op)
- 
-### fit random-effects model in the three subgroups
-res.hp <- rma(yi_SMD_all,vi_SMD_all, subset=(population=="HP"),data=df_meta_analysis)
-res.chr <- rma(yi_SMD_all,vi_SMD_all,subset=(population=="CHR"),data=df_meta_analysis)
-res.p <- rma(yi_SMD_all,vi_SMD_all, subset=(population=="P"),data=df_meta_analysis)
-
-
-### add summary polygons for the three subgroups
-addpoly(res.hp, row=18.5,)
-addpoly(res.chr, row= 7.5, mlab=mlabfun("RE Model for Subgroup", res.chr))
-addpoly(res.p, row= 1.5, mlab=mlabfun("RE Model for Subgroup", res.p))
- 
 
   
-  -## add text for the test of subgroup differences
-#text(-16, -1.8, pos=4, cex=0.32, bquote(paste("Test for Subgroup Differences: ",
-     #Q[M], " = ", .(formatC(rem_unique$QM, digits=2, format="f")), ", df = ", .(rem_unique$p - 1),
-     #", p = ", .(formatC(rem_unique$QMp, digits=2, format="f")))))
